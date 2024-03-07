b78245045c9a7c4d5b0f45f78e00d1815fd9cb47
laiaxarau <laiaxarau@gmail.com>
2024-03-07 20:09:46 +0100

    feat(launches): launches pages

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,7 +1,18 @@
-import type { SpacexRocket } from "../types/types";
+import type { SpacexLaunches } from "../types/launches/types";
+import type { SpacexRocket } from "../types/rockets/types";
 
 export const getRockets = async () => {
   const response = await fetch("https://api.spacexdata.com/v3/rockets");
   const data = (await response.json()) as SpacexRocket[];
   return data;
+};
+
+export const getLaunches = async () => {
+  const response = await fetch("https://api.spacexdata.com/v3/launches");
+  const data = (await response.json()) as SpacexLaunches[];
+
+  return data.filter(launch=> launch.links.mission_patch_small !== null)
+  .sort((a, b) => (
+    b.launch_date_unix - a.launch_date_unix
+  )).slice(0,12);
 };
