4ae37e1c4a53a8d1d8e59704d632a7378f7f5407
laiaxarau <laiaxarau@gmail.com>
2024-03-25 22:15:28 +0100

    wip(home_page): latest news

---
+++ src/types/index/types.ts
@@ -0,0 +1,43 @@
+export type SpacexHistory = {
+    links:           Links;
+    title?:           string;
+    event_date_utc:  Date;
+    event_date_unix: number;
+    details?:         string;
+    id?:              string;
+}
+
+export type Links = {
+    article?: null | string;
+}
+
+export type SpacexInfo = {
+    name:           string;
+    founder:        string;
+    founded:        number;
+    employees:      number;
+    vehicles:       number;
+    launch_sites:   number;
+    test_sites:     number;
+    ceo:            string;
+    cto:            string;
+    coo:            string;
+    cto_propulsion: string;
+    valuation:      number;
+    headquarters:   Headquarters;
+    links:          InfoLinks;
+    summary:        string;
+}
+
+export type Headquarters = {
+    address: string;
+    city:    string;
+    state:   string;
+}
+
+export type InfoLinks = {
+    website:      string;
+    flickr:       string;
+    twitter:      string;
+    elon_twitter: string;
+}
